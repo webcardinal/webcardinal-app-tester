e673b68c644a3351e256f0e95a67abd3dffbc844
Deny Pătrașcu <denypatrascu@gmail.com>
2021-04-08 10:29:48 +0300

    Adding webc-modal

---
+++ scripts/controllers/manual/webc-modal/Test1.js
@@ -0,0 +1,34 @@
+const { WebcController } = WebCardinal.controllers;
+
+export default class extends WebcController {
+    constructor(element, history) {
+        super(element, history);
+
+        this.model = {
+            modal: {
+                title: "My Modal",
+                content: "Hello Content 👋"
+            }
+        };
+
+        this.onTagClick('show-modal', readOnlyModel => {
+            const { modal } = readOnlyModel;
+
+            this.showModalFromTemplate('modal1', this.onConfirm.bind(this), this.onClose.bind(this), {
+                model: modal
+            });
+        });
+
+        setTimeout(() => {
+            this.getElementByTag('show-modal').click();
+        }, 300);
+    }
+
+    onConfirm(event) {
+        console.log('onClose', event)
+    }
+
+    onClose(event) {
+        console.log('onClose', event)
+    }
+}
---
+++ scripts/controllers/manual/webc-modal/Test2.js
@@ -0,0 +1,32 @@
+const { WebcController } = WebCardinal.controllers;
+
+export default class extends WebcController {
+    constructor(element, history) {
+        super(element, history);
+
+        this.model = {
+            modal: {
+                title: "My Modal",
+                content: "Hello Content 👋"
+            }
+        };
+
+        this.onTagClick('show-modal', readOnlyModel => {
+            const { modal } = readOnlyModel;
+
+            // this.showModalFromTemplate('modal1', this.onConfirm.bind(this), this.onClose.bind(this), {
+            //     model: { hello: 'Hello!' },
+            //     translationModel: modal,
+            //     enableTranslations: true
+            // });
+        });
+    }
+
+    onConfirm(event) {
+        console.log('onClose', event)
+    }
+
+    onClose(event) {
+        console.log('onClose', event)
+    }
+}
