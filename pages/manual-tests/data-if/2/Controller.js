8ad6528fb2b13ab81863b6d31823e1244f509f09
Deny Pătrașcu <denypatrascu@gmail.com>
2021-11-15 17:25:20 +0200

    Test for loading slot of data-if

---
+++ pages/manual-tests/data-if/2/Controller.js
@@ -0,0 +1,42 @@
+// Path: pages/manual-tests/data-if/2/Controller.js
+
+const { Controller } = WebCardinal.controllers;
+
+class TestController extends Controller {
+    constructor(...props) {
+        super(...props);
+
+        this.model = {
+            condition: undefined
+        }
+
+        this.model.addExpression("conditionString", () => {
+            return `${this.model.condition}`;
+        }, 'condition');
+
+        this.inerval = setInterval(() => {
+            this.changeCondition();
+        }, 1000);
+    }
+
+    changeCondition() {
+        let newCondition;
+        switch (this.model.condition) {
+            case true:
+                newCondition = false;
+                break;
+            case false:
+                newCondition = undefined;
+                break;
+            default:
+                newCondition = true;
+        }
+        this.model.condition = newCondition;
+    }
+
+    onDisconnectedCallback() {
+        clearInterval(this.inerval);
+    }
+}
+
+export default TestController;
