fdac9282ef7b38e0bbe5ad89440b37a511e03032
Deny Pătrașcu <denypatrascu@gmail.com>
2021-11-29 16:42:51 +0200

    Update WebCardinal

--- webcardinal/components/webcardinal-core/p-7b9c6a6b.js
+++
@@ -1,1 +0,0 @@
-import{N as o,G as t,O as a,a as r,U as s}from"./p-7abb90e5.js";import"./p-e506992e.js";const{join:e}=s,n={getController:async s=>{const{controllers:n,basePath:l}=window.WebCardinal,i=o();if(n[s])return n[s];let c=await t(e(l,a(),r,"controllers",s).pathname);return c?(n[s]=c,c):"default"!==i?(c=await t(e(l,r,"controllers",s).pathname),c?(n[s]=c,c):void 0):void 0}};export{n as C}
