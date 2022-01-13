6262c8c0486e2a0e058022826b0770990307876a
mastaleru <mastaleru.rafael@gmail.com>
2022-01-13 14:21:38 +0200

    data-view-model fails to bind when model is asynchronous initialized

---
+++ scripts/controllers/manual/model/AsyncModelLoadController.js
@@ -0,0 +1,23 @@
+const {WebcController} = WebCardinal.controllers;
+
+export default class AsyncModelLoadController extends WebcController {
+    constructor(...props) {
+        super(...props);
+        this.model.form = {};
+
+        this.model.form.input1 = {
+            type:"text",
+            placeholder:"Enter some data in input #1",
+            value:""
+        }
+
+        setTimeout(()=>{
+            this.model.form.input2 = {
+                type:"text",
+                placeholder:"Enter some data in input #2",
+                value:""
+            }
+        },10)
+
+    }
+}
