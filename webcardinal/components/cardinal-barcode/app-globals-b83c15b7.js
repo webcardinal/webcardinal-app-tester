3edd67f117bbbe33ef70496188c2b768038c4a50
Deny Pătrașcu <denypatrascu@gmail.com>
2022-01-14 13:32:36 +0200

    Update WebCardinal

---
+++ webcardinal/components/cardinal-barcode/app-globals-b83c15b7.js
@@ -0,0 +1,8 @@
+import { e as cardinalInternalsGlobalScript } from './index-f1c4ce5f.js';
+
+const globalScripts = () => {
+  cardinalInternalsGlobalScript();
+  cardinalInternalsGlobalScript();
+};
+
+export { globalScripts as g };
