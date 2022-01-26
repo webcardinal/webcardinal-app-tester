c4d326372f9b8464eff6e0506baffec8146a2f97
Deny Pătrașcu <denypatrascu@gmail.com>
2022-01-26 17:21:46 +0200

    Update psk-barcode-scanner

---
+++ webcardinal/components/cardinal-barcode/app-globals-a8d77a37.js
@@ -0,0 +1,8 @@
+import { e as cardinalInternalsGlobalScript } from './index-cac30108.js';
+
+const globalScripts = () => {
+  cardinalInternalsGlobalScript();
+  cardinalInternalsGlobalScript();
+};
+
+export { globalScripts as g };
