44e1cf7b057cb692071d3da96f46375b237b38f4
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2022-02-24 12:27:54 +0200

    light scanner: sending center area from the video stream to the worker

--- pages/tools/light-scanner/SimpleController.js
+++ pages/tools/light-scanner/SimpleController.js
@@ -38,28 +38,23 @@ export default class SimpleController extends Controller {
 		this.scanning();
 	}
 
+	getCenterArea = () =>{
+		let size = 400;
+		const points = [(1920-size)/2, (1080-size)/2, size, size];
+		return points;
+	}
+
+	drawCenterArea = (context) => {
+		context.lineWidth = 10;
+		const centerAreaPoints = this.getCenterArea();
+		context.strokeRect(...centerAreaPoints);
+	}
+
 	debugFrame = (frame) =>{
 		const debug = this.element.querySelector("#debug");
 		const ctx = debug.getContext("2d");
 
 		ctx.putImageData(frame, 0, 0);
-		ctx.lineWidth = 10;
-		let size = 400;
-		ctx.strokeRect((1920-size)/2, (1080-size)/2, size, size);
-	}
-
-	toGrayscaleBuffer = (imageData, width, height) => {
-		const greyScaleImageData = new Uint8ClampedArray(width * height);
-		for (let e = 0, r = 0, i = imageData.length; e < i; e += 4, r++) {
-			let i;
-			if (0 === imageData[e + 3])
-				i = 255;
-			else {
-				i = 306 * imageData[e] + 601 * imageData[e + 1] + 117 * imageData[e + 2] + 512 >> 10
-			}
-			greyScaleImageData[r] = i
-		}
-		return greyScaleImageData;
 	}
 
 	decode = (imageData) =>{
@@ -152,54 +147,6 @@ export default class SimpleController extends Controller {
 		});
 	}
 
-	sharpen = (ctx, w, h, mix) => {
-		var x, sx, sy, r, g, b, a, dstOff, srcOff, wt, cx, cy, scy, scx,
-			weights = [0, -1, 0, -1, 5, -1, 0, -1, 0],
-			katet = Math.round(Math.sqrt(weights.length)),
-			half = (katet * 0.5) | 0,
-			dstData = ctx.createImageData(w, h),
-			dstBuff = dstData.data,
-			srcBuff = ctx.getImageData(0, 0, w, h).data,
-			y = h;
-
-		while (y--) {
-			x = w;
-			while (x--) {
-				sy = y;
-				sx = x;
-				dstOff = (y * w + x) * 4;
-				r = 0;
-				g = 0;
-				b = 0;
-				a = 0;
-
-				for (cy = 0; cy < katet; cy++) {
-					for (cx = 0; cx < katet; cx++) {
-						scy = sy + cy - half;
-						scx = sx + cx - half;
-
-						if (scy >= 0 && scy < h && scx >= 0 && scx < w) {
-							srcOff = (scy * w + scx) * 4;
-							wt = weights[cy * katet + cx];
-
-							r += srcBuff[srcOff] * wt;
-							g += srcBuff[srcOff + 1] * wt;
-							b += srcBuff[srcOff + 2] * wt;
-							a += srcBuff[srcOff + 3] * wt;
-						}
-					}
-				}
-
-				dstBuff[dstOff] = r * mix + srcBuff[dstOff] * (1 - mix);
-				dstBuff[dstOff + 1] = g * mix + srcBuff[dstOff + 1] * (1 - mix);
-				dstBuff[dstOff + 2] = b * mix + srcBuff[dstOff + 2] * (1 - mix);
-				dstBuff[dstOff + 3] = srcBuff[dstOff + 3];
-			}
-		}
-
-		ctx.putImageData(dstData, 0, 0);
-	}
-
 	captureFrame = () =>{
 		let canvas = this.element.querySelector("#scene");
 		let context = canvas.getContext("2d");
@@ -207,17 +154,17 @@ export default class SimpleController extends Controller {
 		let video = this.element.querySelector("#userFeedback");
 
 		let {width, height} = video.srcObject.getVideoTracks()[0].getConstraints();
-		//let width = video.videoWidth;
-		//let height = video.videoHeight;
+
 		canvas.width = width;
 		canvas.height = height;
 		//context.filter = 'brightness(1.75) contrast(1) grayscale(1)';
-		//context.filter = 'grayscale(1) contrast(1)';
 		context.drawImage(video, 0, 0, width, height);
-		//this.sharpen(context, width, height,  0.9);
+
+		this.drawCenterArea(context);
 
 		context.imageSmoothingEnabled = false;
-		let frameAsImageData = context.getImageData(0, 0, width, height);
+		//let frameAsImageData = context.getImageData(0, 0, width, height);
+		let frameAsImageData = context.getImageData(...this.getCenterArea());
 
 		return frameAsImageData;
 	}
