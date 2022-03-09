a506a8d75592a0627c4f6bacb9f33608bc3f85e6
Deny Pătrașcu <denypatrascu@gmail.com>
2022-03-09 12:22:12 +0200

    Add libs.sh for git initialization of zxing-wrapper

--- libs.sh
+++ libs.sh
@@ -1,7 +1,12 @@
-### script for zxing-wrapper
+#!/bin/sh
 
-```bash
+# Give permission to the following script
+# chmod a+rx
+
+cd libs/zxing-wrapper
+rm -rf .git
 git init
 git remote add origin https://github.com/webcardinal/zxing-wrapper
 git fetch origin && git reset --mixed origin/master && git clean -f -d
-``` 
+
+echo "zxing-wrapper fetched from origin"
