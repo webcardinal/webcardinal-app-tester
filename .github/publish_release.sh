c140e0e672965bb8c3d6fbde1c692eb5ab379877
Deny Pătrașcu <denypatrascu@gmail.com>
2022-03-17 13:09:39 +0200

    Update workflows

--- .github/publish_release.sh
+++ .github/publish_release.sh
@@ -29,7 +29,7 @@ function publish_bundle() {
 
   git add dist/
   git commit -m "WebCardinal release for $bundle (build-id #$GITHUB_RUN_NUMBER)"
-  git push origin "$branch" --set-upstream --repo="https://${GITHUB_TOKEN}@github.com/webcardinal/$repository.git"
+  git push origin "$branch" --repo="https://${GITHUB_TOKEN}@github.com/webcardinal/$repository.git"
 
   rm -rf "$root/temp"
 
@@ -73,4 +73,4 @@ function publish_distribution() {
 
 #publish_distribution "$1"
 
-publish_bundle "webcardinal-test-release" "master" "release/production" "bundle-minimal"
+publish_bundle "webcardinal-minimal-release" "master" "release/production" "bundle-minimal"
