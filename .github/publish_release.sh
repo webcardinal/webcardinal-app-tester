aaf92942c90bce84d3a52fc4cd7750ae09d189ca
Deny Pătrașcu <denypatrascu@gmail.com>
2022-03-17 14:06:34 +0200

    Update workflows

--- .github/publish_release.sh
+++ .github/publish_release.sh
@@ -8,7 +8,7 @@ function publish_bundle() {
   dir=$3
   bundle=$4
 
-  echo "Publishing into repository '$repository' in branch '$branch' from path '$(pwd)'"
+  echo "Publishing into repository '$repository' in branch '$branch' from path '$root'"
 
   mkdir "temp"
   cd "$root/temp" || exit 1
@@ -17,8 +17,11 @@ function publish_bundle() {
 
   echo "Origin: https://$GITHUB_TOKEN@github.com/webcardinal/$repository.git"
 
-  git config user.name "Github Actions"
-  git config user.email "github-actions@users.noreply.github.com"
+#  git config user.name "Github Actions"
+#  git config user.email "github-actions@users.noreply.github.com"
+
+  git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+  git config --local user.name "github-actions[bot]"
 
   git pull origin "$branch" --rebase
 
@@ -31,7 +34,7 @@ function publish_bundle() {
 
   git add dist/
   git commit -m "WebCardinal release for $bundle (build-id #$GITHUB_RUN_NUMBER)"
-  git push origin "$branch" --repo="https://$GITHUB_TOKEN@github.com/webcardinal/$repository.git" -u
+  git push origin "$branch" --repo="https://$GITHUB_TOKEN@github.com/webcardinal/$repository.git"
 
   rm -rf "$root/temp"
 
@@ -75,4 +78,4 @@ function publish_distribution() {
 
 #publish_distribution "$1"
 
-publish_bundle "webcardinal-test-release" "master" "release/production" "bundle-minimal"
+publish_bundle "webcardinal-minimal-release" "master" "release/production" "bundle-minimal"
